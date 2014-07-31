27305223339f5b3aa5d98f3c41f6b32a7a7b0f05
Chris <python490@gmail.com>
2014-07-31 07:48:06 -0700

    Basic Bootstrap site
    desktop and mobile versions working

---
+++ aboutme.php
@@ -0,0 +1,19 @@
+<html>
+<title>About Me</title>
+	<?php  include('include/nav.php');?>
+
+<body class="homepage">
+	<div class="container-fluid">
+		<div class="col-lg-4 col-md-offset-4" style="background-color:white; margin-top:5em;">
+			<p> A little about me here is article <a href="animehistory.php"> My history with Anime</a></p>
+		</div>
+	</div>
+	<div>
+		<p class="text-center">
+		<?php include('include/footer.php');?>
+		</p>
+	</div>
+
+</body>
+
+</html>
---
+++ animecontent1.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<?php include('include/nav.php');?>
+<script type ="text/javascript">
+	$(document).ready(function(){
+		$("#tab li:eq(1) a").tab('show');
+});
+</script>
+
+
+<body class ="reviewpage">
+	<div class="container">
+		<div class ="whitespace2" align = "center">
+			<h1 class ="title">
+			<img class ="banner" src="pics/akame.jpg" alt="Stack Overflow" />
+			</h1>
+			<ul class="nav nav-tabs-animehome" role="tablist" id="tabs">
+			<li class ="active"><a data-toggle="tab" href="#Home">Home</a></li>
+			<li><a data-toggle="tab" href="#Comments">Comments</a></li>
+			</ul>
+			<div id="content" class ="tab-content" >
+	
+				<div id ="Home" class="tab-pane fade in active">
+					<p class ='big'>Maxime Hong Kingston unique biography which bends fact and fiction is a novel of which deals with the “pressure” of being a daughter of Chinese immigrants who emigrated from China. Kingston must juggle between the native “American” culture of her country of birth, but also respect the culture of her ancestry the one her parents still live by and hold to be true. It is this tension in her life that has defined the author and her career because it is the struggle of growing up as a Chinese-American which is one of the quintessential pieces of this autobiography. An example of this cultural “dilemma” that Kingston finds herself trapped in. She describes an instance where a delivery boy made the wrong delivery and her mother wants them to rectify their “crime”.  Kingston enters the store and asks for candy from the store clerk who is baffled by the request, and in response Kingston replies “My mother said you have to give us candy. She said this the way Chinese do it” (Kingston 170). Kingston is uncomfortable because she knows that this is not how things are done in America and from clerk’s point of view they are just seen as beggars. This makes her feel great shame even though the store clerk complies with the request as he gives her candy and this continues anytime she stops by the store from then on. Her mother later states “They understand. You kids just aren't very brave” (171) believing that she had taught them “good manners” and respect for “tradition”, but Kingston knows better and feels that her mother had gotten it completely wrong. It is these type of events that typify the cultural conflict the author has balancing how she has been raise in her home, but at the same time having a growing understanding  of the social interactions of the new country she was born in. The mother or as she is named in the novel Brave Orchard is the most important influence on the author's life and many of the key events, beliefs, and struggles are tied to her. Her mother tells her stories or as the author describes them “talk-stories”, and these stories serve as a way of teaching the Chinese culture to Kingston and her mother’s beliefs as well.  To respect the community, respect the elders, honor the family, and only speak when required to do so. The opening sentences of the novel is an insight into this as it reads “You must not tell anyone” my mother said, “what I am about to tell you. In China your father had a sister who killed herself. She jumped into the family well. We say that your father has all brothers because it is as if she had never been born” (3). The author retells a story that she learned from her mother of a how her aunt had affair with a man, but was not married. Gave birth to a child adding another mouth to feed thus becoming a burden to the family that was already struggling with the village harvest, and later the author describes a raid that targeted her aunt  that took place in the middle of the night condemning her actions.(4-14).  She learns early on that there is a certain “Chinese way” of conducting yourself, and this often conflicts with the world around her as she wants to speak but at the same time she is always told to keep silent and respect tradition. Yet what is this tradition? Is the main question being raised as Kingston is living in a country that is not China and is receiving an education therefore is learning English in the American education system. It is this difference of culture that is the subject of misunderstanding, and conflict within the story for it causes great stress. Exemplified by Brave Orchard (Kingston's mother) as she tells the Kingston’s brother, “What do you know about Chinese business ” (151) as her brother, mother, and aunt were driving to Los Angeles to confront the husband of Kingston aunt Moon Orchard her mothers sister. In the belief that the husband would expect her back and have her take her place as his wife again even though the brother had protested, “Mother this is ridiculous, this whole thing is ridiculous” (150). He ended up being correct as the author notes the plan did not succeeded, and thus part of the struggle within the author is the dichotomy that her mother is stuck in between where she actually lives and the culture she grew up living in. Her mother wishes to keep her tradition and culture alive through her children, but at the same time recognizes that they are perhaps more “American” than they are Chinese. There is frustration on both parts because on one hand they are family, but at the same time they cannot comprehend each other at times. As her mother notes, “You're always listening to Teacher Ghosts, those Scientist Ghosts, Doctor Ghosts” (102) these “Ghosts” being the American’s Kingston meets in her life in California. The people that are showing her a different world than the home she learns about in her own home. </p>
+				</div>
+	
+				<div id="Comments" class="tab-pane fade">
+				<div id="disqus_thread"></div>
+				<script type="text/javascript">
+					/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+					var disqus_shortname = 'code2heaven'; // required: replace example with your forum shortname
+		
+					/* * * DON'T EDIT BELOW THIS LINE * * */
+					(function() {
+					var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+					dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+					(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+				})();
+				</script>
+				<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+				<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+    
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
