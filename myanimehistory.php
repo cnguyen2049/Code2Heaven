fdcb22a01ab6a2d5f9cf9bb4a4471303f62647ca
Chris <python490@gmail.com>
2014-08-08 16:53:41 -0700

    new content
    Anime history php file

---
+++ myanimehistory.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html>
+<head>
+<title>My Anime History</title>
+<meta charset="utf-8">
+<meta name ="description" content="Python490 History with Anime">
+<meta name="keywords" content="Python490's Anime History">
+<meta name="author" content="python490">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php include('include/nav.php');?>
+</head>
+<body class ="reviewpage">
+	<div class="container">
+	<div class ="row">
+		<div class="col-md-6  col-md-offset-3" style="background-color:white; margin-top:3em;">
+					<h1>
+					<img class ="in-text" src="pics/animeH/gundam.jpg" alt="Anime" />
+					</h1>
+					<br>
+			
+			<ul class="nav nav-tabs-animehome" role="tablist" id="tabs">
+			<li class ="active"><a data-toggle="tab" href="#Post">Post</a></li>
+			<li><a data-toggle="tab" href="#Comments">Comments</a></li>
+			</ul>
+		
+			<div id="content" class ="tab-content" >
+	
+				<div id ="Post" class="tab-pane fade in active">
+					
+					<h2 align="center">My history with Anime</h2>
+					<p class="space">
+						Anime is something that is very much apart of my growing up experience. Being born in the early 1990's I remember some flashes of 
+						<a href="http://myanimelist.net/anime/2268/Transformers_Generation_1" target="_blank">Transfomers G1</a> and 
+						<a href="http://myanimelist.net/anime/1264/Yoroiden_Samurai_Troopers" target="_blank">Ronin Warriors</a>. Yet it was 
+						like many of my generation it was <a href="http://myanimelist.net/anime/527/Pokemon" target="_blank">Pokemon</a> and 
+						<a href="http://myanimelist.net/anime/813/Dragon_Ball_Z_(Original)"target="blank">DragonBall Z</a> that got me in full force. 
+						I still remember collecting those Pokemon cards and correcting my relatives that it's "Pokemon" and not "Pokeman". 
+						I must have spent hours on my Game Boy playing Pokemon Red and Blue. DBZ was something I was really passionate about, and perhaps the only show 
+						I consistently followed all the way until it got to <a href="http://myanimelist.net/anime/225/Dragon_Ball_GT" target="_blank">GT</a>(I had to watch that later). 
+					</p>
+					
+					
+					<p class="space">
+						When I moved to Spain for a couple of years the shows I vividly remember are <a href="http://myanimelist.net/anime/530/Bishoujo_Senshi_Sailor_Moon" target="_blank">Sailor Moon</a> 
+						and <a href="http://myanimelist.net/anime/182/Tenkuu_no_Escaflowne" target="_blank">Escaflowne</a> (makes me wonder if mostly girls were watching in Spain) alongside my weekly viewing 
+						of <a href="http://myanimelist.net/anime/552/Digimon_Adventure" target="_blank">Digimon</a> another show I was really into for a long time. Once I got back to the 
+						States it was perfect timing with <a href="http://video.adultswim.com/toonami/" target="_blank">Toonami</a> making it's first go around, and thus I continued to watch DBZ and  
+						<a href="http://myanimelist.net/anime/90/Mobile_Suit_Gundam_Wing" target="_blank">Gundam Wing</a>(later <a href="http://myanimelist.net/anime/96/Mobile_Fighter_G_Gundam" target="_blank">G Gundam</a> 
+						as well).  
+					</p>
+					<br>
+					<img class ="in-text" src="http://www.evamonkey.com/randomimages/eva_on_adult_swim.gif" alt="Adult Swim" />
+					<br>
+					<p class="space">
+						If my memory  serves me correctly <a href="http://www.adultswim.com/" target="_blank">Adult Swim</a> was becoming a thing, and I would try to stay up late and 
+						sneak to watch some TV( didn't work most of the time). This is where I was introduced to a bit of <a href="http://myanimelist.net/anime/93/Mobile_Suit_Gundam_Seed" target="_blank">Gundam Seed</a>, 
+						<a href="http://myanimelist.net/anime/249/InuYasha" target="_blank">Inuysha</a>, and <a href="http://myanimelist.net/anime/30/Neon_Genesis_Evangelion" target="_blank">Evangelion</a>
+						(I would later ended watching all of them in completion except Inuyasha). Also it just occurred to me as I am writing this I have memories of watching 
+						<a href="http://myanimelist.net/anime/763/Zoids" target="blank">Zoids</a>( must have watched that at some point). 
+					</p>
+					
+					<p class="space">
+						Now at this point it becomes cumbersome to stay up late, and watch anime luckily the timing couldn't have been better, and soon I would be getting my own computer in
+						my room before I would have to do my homework on the family computer in the living room( this was a big hassle). I would begin to turn to the internet to get my viewing of my favorite shows plus more. I would find hosting sites, and view what I could on the limited quality that existed at the time. I started to go to the bookstores and libraries, and got into reading <a href="http://en.wikipedia.org/wiki/Manga" target="_blank">manga</a> at that time as well. It was during this time that I remember watching <a href="http://myanimelist.net/anime/199/Sen_to_Chihiro_no_Kamikakushi" target="_blank">Spirited Away</a> ,and going to my local <a href="http://en.wikipedia.org/wiki/Blockbuster_LLC" target="_blank">Blockbuster</a>(yes that long ago) and checking out <a href="http://myanimelist.net/anime/1088/Macross" target="_blank">Macross</a>(Robotech in the store). It was a very exciting time for me I remember watching them and waiting hopelessly for the next time my mother would take me to Blockbuster to pick up the next episodes. 
+					</p>
+					<br>
+					<img class ="in-text" src="http://www.live-evil.org/wp-content/uploads/2013/01/macross-dyrl.png" alt="Bakemonogatari" />
+					<br>
+					<p class="space">
+						By this time my sister had gotten older, and  I had a really small TV in my room where we would watch <a href="http://myanimelist.net/anime/20/Naruto" target="_blank">Naruto</a> there(well she would watch I had my head turned half the time browsing the web, but the TV was so close I heard all the <q>amazing dialogue</q>).I really do mean a small TV imagine a television typical of the 1990's standard  definition, and not even 11 inches in width or height. Looking back I don't know how I survived I even played all my PS2 games on that monitor.
+					</p>
+					
+					<p class="space">
+						Continuing with the story I don't know when exactly the original <a href="http://www.crunchyroll.com/" target="_blank">Crunchyroll</a> came about, but it was around this time I started viewing more, and more shows(not going to list them because by this point if your reading this your most probably into anime and already know most of the 2005+ stuff, or at least a decent amount of it). My story with Anime is most probably in a lot of ways very similar with those of my own generation. It's all I can recall at this moment knowing me I most probably missed out some
+						very important details. 
+					</p>
+					<br>
+					<br>
+					<p align="center"><time pubdate datetime="2014-08-06"><i>7/6/2014</i></time></p>
+					<?php include('include/footer.php');?>
+				</div>
+				<div id="Comments" class="tab-pane fade">
+				<div id="disqus_thread"></div>
+				<script type="text/javascript">
+					/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+					var disqus_shortname = 'code2heaven'; // required: replace example with your forum shortname
+					var disqus_identifier = 'myanimehistory';
+					var disqus_url = 'http://code2heaven.com/myanimehistory.php';
+					var disqus_title = 'My Anime History';
+					/* * * DON'T EDIT BELOW THIS LINE * * */
+					(function() {
+					var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+					dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+					(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+				})();
+				</script>
+				
+				<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+				<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+				
+				
+				</div>
+			</div>
+		</div>
+			
+	</div>
+	</div>
+</body>
+</html>
